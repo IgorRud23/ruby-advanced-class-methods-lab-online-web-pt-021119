20826d67b8fe8bdb128fe2d325b9f05c9af595d3
Igor Rud <rud.igor23@gmail.com>
2019-03-15 00:45:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,11 +36,11 @@ def self.find_or_create_by_name(name)
   self.find_by_name(name) || self.create_by_name(name)
 end
 
-def self.alphabetical(name)
+def self.alphabetical
 @@all.sort_by {|a| a.name}
 end
 
-def self.new_from_filename
+def self.new_from_filename(name)
   song = self.new
   song.name = (name.split(" - ")[1].chomp(".mp3"))
   song.artist_name = (name.split(" - ")[0])
